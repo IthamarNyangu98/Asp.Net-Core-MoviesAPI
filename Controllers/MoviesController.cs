8c95bbdb8e8cef98f1a27749965c50bb09dd8f15
Ithamar Nyangu <www.nyanguithamar@gmail.com>
2023-10-05 10:13:25 +0200

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieAPI.Data;
+using MovieAPI.Models;
+
+namespace MovieAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieAPIContext _context;
+
+        public MoviesController(MovieAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+          if (_context.Movie == null)
+          {
+              return NotFound();
+          }
+            return await _context.Movie.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+          if (_context.Movie == null)
+          {
+              return NotFound();
+          }
+            var movie = await _context.Movie.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> CreateMovie(Movie movie)
+        {
+          if (_context.Movie == null)
+          {
+              return Problem("Entity set 'MovieAPIContext.Movie'  is null.");
+          }
+            _context.Movie.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if (_context.Movie == null)
+            {
+                return NotFound();
+            }
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return (_context.Movie?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/MovieAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieAPI.Data
+{
+    public class MovieAPIContext : DbContext
+    {
+        public MovieAPIContext(DbContextOptions<MovieAPIContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie {get; set;}
+    }
+}
